f81c905da9bc271a5eb6fb49e7679c98a052271f
chriswebb09 <chris.webb5249@gmail.com>
2016-07-06 13:21:18 -0400

    new code

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -3,4 +3,11 @@ name.uppercaseString
 
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
-// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+//prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+name.lowercaseString
+
+name.characters
+
+name.hashValue
+
